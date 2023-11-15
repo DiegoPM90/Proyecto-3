eb8a6757b6bfeb1ce55278f228d00dab1ad42673
unknown <jpp.valdes@gmail.com>
2023-11-15 02:44:57 -0300

    estilos avance

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const input = document.getElementById("txt");
 const solicitar = document.getElementById("solicitar");
 const ctx = document.getElementById("myChart");
-const crear = document.getElementById ("crearTarjeta");
-
+const crear = document.getElementById("crearTarjeta");
 
 const urlApi = "https://huachitos.cl/api/animales";
 // const urlApiTipo = `https://huachitos.cl/api/animales/tipo/${tipoAnimal}`
@@ -12,22 +10,23 @@ const obtenerDatos = async (e) => {
   const response = await fetch(urlApi);
   const respuestaJson = await response.json();
   console.log(respuestaJson);
- 
-  const imagen = respuestaJson.data.map ((elemento)=> elemento.imagen)
-  const esterilizado = respuestaJson.data.map ((elemento)=> elemento.esterilizado)
-  const vacunas = respuestaJson.data.map ((elemento)=> elemento.vacunas)
-  const edad = respuestaJson.data.map((elemento) => pasaredadAmeses(elemento.edad));
+
+  const imagen = respuestaJson.data.map((elemento) => elemento.imagen);
+  const esterilizado = respuestaJson.data.map(
+    (elemento) => elemento.esterilizado
+  );
+  const vacunas = respuestaJson.data.map((elemento) => elemento.vacunas);
+  const edad = respuestaJson.data.map((elemento) =>
+    pasaredadAmeses(elemento.edad)
+  );
   const nombre = respuestaJson.data.map((elemento) => elemento.nombre);
 
-  
+  crearGrafico(edad, nombre, vacunas, esterilizado);
 
-  
-  crearGrafico(edad, nombre,vacunas,esterilizado)
-  crear.innerHTML = ''
-  respuestaJson.data.map(element => {
-  crear.innerHTML += creartarjeta (element)
+  crear.innerHTML = "";
+  respuestaJson.data.map((element) => {
+    crear.innerHTML += creartarjeta(element);
   });
-  
 };
 
 const pasaredadAmeses = (edad) => {
@@ -39,7 +38,7 @@ const pasaredadAmeses = (edad) => {
   return parseInt(arrayEdad[0]);
 };
 
-const crearGrafico = (datos, etiquetas,vacunas,esterilizado) => {
+const crearGrafico = (datos, etiquetas, vacunas, esterilizado) => {
   new Chart(ctx, {
     type: "bar",
     data: {
@@ -49,13 +48,13 @@ const crearGrafico = (datos, etiquetas,vacunas,esterilizado) => {
           label: "Edad en meses",
           data: datos,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(201, 203, 207, 0.2)",
+            "rgba(255, 99, 132, 100)",
+            "rgba(255, 159, 64, 100)",
+            "rgba(255, 205, 86, 100)",
+            "rgba(75, 192, 192, 100)",
+            "rgba(54, 162, 235, 100)",
+            "rgba(153, 102, 255,100)",
+            "rgba(201, 203, 207,100)",
           ],
           borderWidth: 2,
         },
@@ -63,27 +62,27 @@ const crearGrafico = (datos, etiquetas,vacunas,esterilizado) => {
           label: "Vacunas",
           data: vacunas,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(201, 203, 207, 0.2)",
+            "rgba(255, 99, 132, 100)",
+            "rgba(255, 159, 64, 100)",
+            "rgba(255, 205, 86, 100)",
+            "rgba(75, 192, 192, 100)",
+            "rgba(54, 162, 235, 100)",
+            "rgba(153, 102, 255,100)",
+            "rgba(201, 203, 207,100)",
           ],
-          borderWidth: 1,
+          borderWidth: 2,
         },
         {
           label: "Esterilizado",
           data: esterilizado,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(201, 203, 207, 0.2)",
+            "rgba(255,99,132,100)",
+            "rgba(255,159,64,100)",
+            "rgba(255,205,86,100)",
+            "rgba(75,192,192,100)",
+            "rgba(54,162,235,100)",
+            "rgba(153,102,255,100)",
+            "rgba(201,203,207,100)",
           ],
           borderWidth: 1,
         },
@@ -98,18 +97,28 @@ const crearGrafico = (datos, etiquetas,vacunas,esterilizado) => {
     },
   });
 };
-
-const  creartarjeta = (data) =>{
+{
+  /* <div class:"tarjeta"> */
+}
+const creartarjeta = (data) => {
   const tarjeta = `
-  <div class:'tarjeta'>
+  <div class="tarjetaInner">
+ 
   <img src= ${data.imagen} alt="">
-  
-  <h3>${data.nombre}</h3>
-  </div>
-  `
-  return tarjeta
-  }
-  
 
+<div class="tarjetaBottom">
+<div class="textosBottom">
+
+  <h3 class="textoTarjeta">${data.nombre}</h3>
+  <h3 class="textoTarjeta">${data.edad}</h3>
+  <h3 class="textoTarjeta">${data.comuna}</h3>
+  <h3 class="textoTarjeta">${data.desc_adicional}</h3>
+  
+  </div>
+  </div>
+  </div>
+  `;
+  return tarjeta;
+};
 
 solicitar.addEventListener("click", obtenerDatos);
