3fb5e5c8bcbb1aa35a36529d824f4c340333bbbe
Sebastián A. López <140351890+sebaao@users.noreply.github.com>
2023-11-17 10:41:44 -0300

    Update main buscador.js

--- js/main buscador.js
+++ js/main buscador.js
@@ -1,148 +1,4 @@
-const solicitar = document.getElementById("solicitar");
-const ctx = document.getElementById("myChart");
-const crear = document.getElementById("crearTarjeta");
-
-const urlApi = "https://huachitos.cl/api/animales";
-// const urlApiTipo = `https://huachitos.cl/api/animales/tipo/${tipoAnimal}`
-
-
-// FUNCION PARA FETCH
-const obtenerDatos = async () => {
-  // e.preventDefault(); /* desactive el boton, ya que el landing ya tiene uno */
-  const response = await fetch(urlApi);
-  const respuestaJson = await response.json();
-  console.log(respuestaJson);
-  const imagen = respuestaJson.data.map((elemento) => elemento.imagen);
-  const esterilizado = respuestaJson.data.map(
-    (elemento) => elemento.esterilizado
-  );
-  const vacunas = respuestaJson.data.map((elemento) => elemento.vacunas);
-  const edad = respuestaJson.data.map((elemento) =>
-    pasaredadAmeses(elemento.edad)
-  );
-  const nombre = respuestaJson.data.map((elemento) => elemento.nombre);
-
-  crearGrafico(edad, nombre, vacunas, esterilizado);
-  crear.innerHTML = "";
-  respuestaJson.data.map((element) => {
-    crear.innerHTML += creartarjeta(element);
-  });
-};
-
-
-
-
-const crearGrafico = (datos, etiquetas, vacunas, esterilizado) => {
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: etiquetas,
-      fontColor: 'blue',
-      datasets: [
-        {
-          label: "Edad en meses",
-          data: datos,
-          borderColor: '#111111',
-          backgroundColor: [
-            "rgba(255, 99, 132, 100)",
-            "rgba(255, 159, 64, 100)",
-            "rgba(255, 205, 86, 100)",
-            "rgba(75, 192, 192, 100)",
-            "rgba(54, 162, 235, 100)",
-            "rgba(153, 102, 255,100)",
-            "rgba(201, 203, 207,100)",
-          ],
-          borderWidth: 1,
-        },
-        {
-          label: "Vacunas",
-          data: vacunas,
-          borderColor: '#111111',
-          backgroundColor: [
-            "rgba(255, 99, 132, 100)",
-            "rgba(255, 159, 64, 100)",
-            "rgba(255, 205, 86, 100)",
-            "rgba(75, 192, 192, 100)",
-            "rgba(54, 162, 235, 100)",
-            "rgba(153, 102, 255,100)",
-            "rgba(201, 203, 207,100)",
-          ],
-          borderWidth: 1,
-        },
-        {
-          label: "Esterilizado",
-          data: esterilizado,
-          borderColor: '#111111',
-          backgroundColor: [
-            "rgba(255,99,132,100)",
-            "rgba(255,159,64,100)",
-            "rgba(255,205,86,100)",
-            "rgba(75,192,192,100)",
-            "rgba(54,162,235,100)",
-            "rgba(153,102,255,100)",
-            "rgba(201,203,207,100)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-};
-const pasaredadAmeses = (edad) => {
-  const arrayEdad = edad.split(" ");
-  console.log(arrayEdad);
-  if (arrayEdad[1] === "Años" || arrayEdad[1] === "Año") {
-    return parseInt(arrayEdad[0]) * 12;
-  }
-  return parseInt(arrayEdad[0]);
-};
-
-const creartarjeta = (data) => {
-  const tarjeta = `
-  <div class="tarjetaInner">
- 
-  <img src= ${data.imagen} alt="">
-
-<div class="tarjetaBottom">
-<div class="textosBottom">
-
-  <h2 class="textoTarjeta">${data.nombre}</h2>
-  <p>&nbsp;</p>
-  <h3 class="textoTarjeta">${data.edad}</h3>
-  <h3 class="textoTarjeta">${data.comuna}</h3>
-  <h3 class="textoTarjeta">${data.desc_personalidad}</h3>
-  </div>
-
-  </div>  
-  <div class="divWsp">  
-  <a target="_blank" href="https://wa.me/+56975467484" class="buttonWsp">
-  <i class="wp">¡Contacta a ${data.nombre}!</i>
-  <i class="fab fa-whatsapp wp"></i>
-</a>
-  </div>
-  </div>
-  
-  `;
-  return tarjeta;
-};
-
-obtenerDatos()
-// solicitar.addEventListener("click", obtenerDatos);
-
-
-
-
-
-
-
-// Graficos Finales
+// Graficos Finales (Perros vs Gatos, Esterilizados, Edad)
 
 document.addEventListener('DOMContentLoaded', function() {
   const urlApi = 'https://huachitos.cl/api/animales';
@@ -278,6 +134,100 @@ new Chart(ctxEdad, {
 
   iniciar(); // Iniciar el proceso al cargar la página
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tarjeta de Animales
+
+
+// URL de la API para obtener datos de animales
+const urlApi = "https://huachitos.cl/api/animales";
+
+// Función principal para obtener los datos de la API y generar tarjetas
+const obtenerDatos = async () => {
+  try {
+    // Hacer la petición a la API
+    const response = await fetch(urlApi);
+    const respuestaJson = await response.json();
+    console.log(respuestaJson);
+    
+    // Extraer datos relevantes de la respuesta JSON
+    const { data } = respuestaJson;
+
+    // Limpiar las tarjetas existentes
+    limpiarTarjetas();
+
+    // Crear tarjetas con los datos obtenidos de la API
+    data.forEach((element) => {
+      crearTarjeta(element);
+    });
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+  }
+};
+
+// Función para crear una tarjeta con información del animal y agregarla al HTML
+const crearTarjeta = (data) => {
+  const tarjeta = `
+    <div class="tarjetaInner">
+      <img src="${data.imagen}" alt="">
+      <div class="tarjetaBottom">
+        <div class="textosBottom">
+          <h2 class="textoTarjeta">${data.nombre}</h2>
+          <p>&nbsp;</p>
+          <h3 class="textoTarjeta">${data.edad}</h3>
+          <h3 class="textoTarjeta">${data.comuna}</h3>
+          <h3 class="textoTarjeta">${data.desc_personalidad}</h3>
+        </div>
+      </div>  
+      <div class="divWsp">  
+        <a target="_blank" href="https://wa.me/+56975467484" class="buttonWsp">
+          <i class="wp">¡Contacta a ${data.nombre}!</i>
+          <i class="fab fa-whatsapp wp"></i>
+        </a>
+      </div>
+    </div>
+  `;
+  const crear = document.getElementById("crearTarjeta");
+  crear.innerHTML += tarjeta;
+};
+
+// Función para limpiar las tarjetas existentes
+const limpiarTarjetas = () => {
+  const crear = document.getElementById("crearTarjeta");
+  crear.innerHTML = "";
+};
+
+// Ejecutar la función principal para obtener datos y generar tarjetas
+obtenerDatos();
+// solicitar.addEventListener("click", obtenerDatos);
+
 
 
 
